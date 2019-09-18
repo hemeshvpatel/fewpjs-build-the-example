b18a10b70a4c4bdc11494eeae09e0882bcf73737
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-18 17:32:51 -0500

    Done.

--- main.js
+++ main.js
@@ -2,9 +2,53 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const heartButtons = document.querySelectorAll('.like-glyph')
+const modal = document.getElementById("modal")
+const modalMessage = document.getElementById("modal-message")
+
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("*The page has now loaded*")
 
+  //add .hidden class to error modal in HTML so it does not appear when page first loads
+  modal.hidden = true;
+
+  //click event triggers mimicServerCall and empty/full heart change
+
+  for(const heartButton of heartButtons) {
+      //add event trigger for each button
+      heartButton.addEventListener("click", (event) => {
+        console.log(mimicServerCall())
+        // when a click is received, generate a server call
+        mimicServerCall()
+        .then((event) => {
+          //console.log("~! I just clicked a heart button ! ~")
+          //console.log("heart button inner text= ", heartButton.innerText)
+          if (heartButton.innerText === EMPTY_HEART) {
+            heartButton.innerText = FULL_HEART;
+            //change heart to red
+            heartButton.classList.add('activated-heart')
+          } else {
+            heartButton.innerText = EMPTY_HEART;
+             //change heart back to blank
+            heartButton.classList.remove('activated-heart')
+          }
+          //console.log("heart button changed to= ", heartButton.innerText)
+        })
+        .catch((error) => {
+          //Display the error modal by removing the .hidden class
+          modal.hidden = false; 
+          modalMessage.innerText = error;
+          console.log(modal.hidden, error)
+          //Use setTimeout to hide the modal after 5 seconds (add the .hidden class)
+          setTimeout( (event) => { 
+            modal.hidden = true;
+          }, 5000);
+        })  
+    })
+  }
+})
 
 
 //------------------------------------------------------------------------------
